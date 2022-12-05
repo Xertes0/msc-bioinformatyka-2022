46a244aa533054b05042f5a3e62d50e2e79cae6c
Jakub Lis <jakublis@vp.pl>
2022-12-05 16:52:19 +0100

    shorten skeletal timeout to 100ms

--- web/src/react_components/SkeletalFormula/SkeletalFormula.tsx
+++ web/src/react_components/SkeletalFormula/SkeletalFormula.tsx
@@ -22,27 +22,28 @@ export default function SkeletalFormula() {
     }, []);
     return (
         bioModuleLoaded ?
-        <div className={"SkeletalFormula"}>
-            <h1>Skeletal Formula</h1>
-            <div id="svgDiv" style={{height: "45vh", maxWidth: "1000vw", margin: "auto", backgroundColor: "white"}} dangerouslySetInnerHTML={
-                function() {
-                    let svg = document.createElement("svg");//"<svg width='100%' height='100%' id='aa_svg'" + " xmlns='http://www.w3.org/2000/svg'></svg>");// + bioModule.bio_draw_skeletal(formula) + "</svg>");
-                    svg.setAttribute("width", "100%");
-                    svg.setAttribute("height", "100%");
-                    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-                    // @ts-ignore
-                    svg.innerHTML = bioModule.bio_draw_skeletal(formula);
-                    setTimeout(()=>{
-                        // @ts-ignore
-                        let svg = document.getElementsByTagName("svg")[0];
-                        let bbox: SVGRect = svg.getBBox();
-                        // @ts-ignore
-                        svg.setAttribute("viewBox", `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height + bbox.y}`)
-                    }, 1000);
-                    return {__html: svg.outerHTML};
-                }()
-            }>
-            </div>
-        </div> : <div>Loading...</div>
+            <div className={"SkeletalFormula"}>
+                <h1>Skeletal Formula</h1>
+                <div id="svgDiv" style={{height: "45vh", maxWidth: "1000vw", margin: "auto", backgroundColor: "white"}}
+                     dangerouslySetInnerHTML={
+                         function () {
+                             let svg = document.createElement("svg");//"<svg width='100%' height='100%' id='aa_svg'" + " xmlns='http://www.w3.org/2000/svg'></svg>");// + bioModule.bio_draw_skeletal(formula) + "</svg>");
+                             svg.setAttribute("width", "100%");
+                             svg.setAttribute("height", "100%");
+                             svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+                             // @ts-ignore
+                             svg.innerHTML = bioModule.bio_draw_skeletal(formula);
+                             setTimeout(() => {
+                                 // @ts-ignore
+                                 let svg = document.getElementsByTagName("svg")[0];
+                                 let bbox: SVGRect = svg.getBBox();
+                                 // @ts-ignore
+                                 svg.setAttribute("viewBox", `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height + bbox.y}`)
+                             }, 100);
+                             return {__html: svg.outerHTML};
+                         }()
+                     }>
+                </div>
+            </div> : <div>Loading...</div>
     )
 }
