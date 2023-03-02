37147342e33d4beb5e6cb9a247713e6cb3f1fcbf
Jonatan Czarniecki <czarniecki.jonatan@gmail.com>
2023-03-02 23:39:31 +0100

    add input description

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -36,8 +36,20 @@ function App() {
     return (
         <div className="App">
             <div className="card">
-                <h1>Translate a sequence</h1>
-                { bioModule && <div className="card"> <SequenceInput onSubmit={submit} /></div> }
+                <h1>Input a nucleic acid sequence:</h1>
+                { bioModule && <div className="form-container">
+                    <SequenceInput onSubmit={submit} />
+                    <div className="form-description-container">
+                        <h2>The input accepts nucleobases mapped as follows:</h2>
+                        <ul>
+                            <li>A = Adenine</li>
+                            <li>C = Cytosine</li>
+                            <li>G = Guanine</li>
+                            <li>T = Thymine</li>
+                            <li>U = Uracil</li>
+                        </ul>
+                    </div>
+                </div> }
                 <div onClick={orfClick}>{ orfs.map((orf, index) => <OpenReadingFrame key={index} id={index} content={orf}/>) }</div>
             </div>
         </div>
