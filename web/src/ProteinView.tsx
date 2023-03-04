a93b9e81b471ca09705caecf6af483324a32f457
Jonatan Czarniecki <czarniecki.jonatan@gmail.com>
2023-03-04 19:13:20 +0100

    update the design of ProteinView

--- web/src/ProteinView.tsx
+++ web/src/ProteinView.tsx
@@ -4,15 +4,23 @@ import "./ProteinView.css"
 
 export interface FoundProteinsViewProps {
   sequence: string;
-  onProteinClick(event: ProteinClick): void;
+  onProteinClick(event: OpenReadingFrameClick): void;
 }
 
-export interface ProteinClick extends SyntheticEvent<HTMLTableRowElement> {
+export interface OpenReadingFrameClick extends SyntheticEvent<HTMLTableRowElement> {
   target: HTMLTableRowElement;
   sequence: string;
 }
 
-function ProteinView({ onProteinClick }: FoundProteinsViewProps) {
+export class OpenReadingFrame {
+  nbStart?: number;
+  nbEnd?: number;
+  nbShift?: 0 | 1 | 2 | undefined;
+  aaStart?: number;
+  aaEnd?: number;
+}
+
+function ProteinView({ sequence, onProteinClick }: FoundProteinsViewProps) {
 
   const [bioModule, setBioModule] = useState<null | BioModule>(null);
 
@@ -22,35 +30,82 @@ function ProteinView({ onProteinClick }: FoundProteinsViewProps) {
     })
   }, []);
 
-  //TODO: get data from props
-  const sequence = "gatcgtcagatgtcagtacgtcagtcatgaacgcatgcgtacgacgtagcacgt".toUpperCase();
-  const foundProteins = [[12, 20], [4, 23], [5, 12], [0, 54]]; // bioModule?.translate(sequence) || [];
-  const highlightColors = foundProteins.map((_, i) => `hsl(${((i + 1) / foundProteins.length * 360) + 40}, 100%, 50%)`);
-  const [activeProteinIdx, setActiveProteinIdx] = useState<undefined | number>(undefined);
+  const result = bioModule?.translate(sequence);
 
-  return (
+  const translatedSequence: [string, string, string] =
+    result?.map((v) => v.sequence) as [string, string, string] || []
+
+  const openReadingFrames =
+    bioModule?.translate(sequence)?.map((result, resultIdx) => {
+      let indices: OpenReadingFrame[] = [];
+      for (let i = 0; i < result.indices.size(); i++) {
+        const {end, start} = result.indices.get(i);
+        indices[i] = Object.assign(new OpenReadingFrame(), {
+          nbStart: start * 3 + resultIdx,
+          nbEnd: end * 3 + resultIdx,
+          nbShift: resultIdx,
+          aaStart: start,
+          aaEnd: end,
+        });
+      }
+      return indices;
+    }) as [OpenReadingFrame[], OpenReadingFrame[], OpenReadingFrame[]] || [];
+
+  const openReadingFramesCount = openReadingFrames?.flat().reduce(r => r + 1, 0);
+
+  const higlightColors = Array.from(
+    { length: openReadingFramesCount },
+    (_, i) => `hsl(${i / openReadingFramesCount * 360 + 60}, 100%, 50%)`
+  );
+
+  const [activeOpenReadingFrame, setActiveOpenReadingFrame] = useState<undefined | OpenReadingFrame>(undefined);
+
+  let orfs = -1;
+  return bioModule && openReadingFrames ?
     <table className="protein-view">
       <thead>
-        <tr>
-          {Array.from(sequence).map((c, i) => <th key={i}>{c}</th>)}
+        <tr className="nb-seq header">
+          { Array.from(sequence).map((c, ci) => <th key={ci} className="nb" data-idx={ci}>{c}</th>) }
         </tr>
       </thead>
       <tbody>
-      {
-        foundProteins.map((t, i) => (
-          <tr key={i} className={i == activeProteinIdx ? "active" : undefined} onClick={(event: React.MouseEvent<HTMLTableRowElement>) => {
-            const newEvent = {...event, target: event.currentTarget as HTMLTableRowElement, sequence: sequence.slice(...t)};
-            setActiveProteinIdx(newEvent.target.classList.contains("active") ? undefined : i);
-            onProteinClick(newEvent);
-          }}>
-            {t[0] > 0 && <td colSpan={t[0]}></td>}
-            {Array.from(sequence).slice(...t).map((c, ci) => <td key={ci} style={{ borderColor: highlightColors[i] }}>{c}</td>)}
-          </tr>
-        ))
-      }
+        {
+          translatedSequence.map((seq, seqIdx) =>
+            (
+              <>
+                <tr key={`seq-${seqIdx}`} className="aa-seq header" data-shift={seqIdx} data-seq={seq}>
+                  {seqIdx > 0 && <th colSpan={seqIdx}></th>}
+                  {Array.from(seq).map((aa, aaIdx) => <th key={aaIdx} colSpan={3} className="aa"
+                                                          data-idx={aaIdx}>{aa}</th>)}
+                </tr>
+                {openReadingFrames[seqIdx].map((orf, orfIdx) => {
+                  orfs++;
+                  return (
+                    <tr key={`orf-${orfIdx}`}
+                        className={`orf${
+                          activeOpenReadingFrame?.nbStart == orf.nbStart &&
+                          activeOpenReadingFrame?.nbEnd == orf.nbEnd
+                            ? " active" : ""
+                        }`}
+                        data-shift={seqIdx}
+                        data-orf={JSON.stringify(orf)}>
+                      {(orf.nbStart || 0) > 0 && <td colSpan={orf.nbStart}></td>}
+                      {Array.from(seq.slice(orf.aaStart, orf.aaEnd)).map((aa, aaIdx) =>
+                        <td key={aaIdx}
+                            colSpan={3}
+                            className="aa"
+                            data-idx={aaIdx}
+                            style={{borderColor: higlightColors[orfs]}}>{aa}</td>
+                      )}
+                    </tr>
+                  );
+                })}
+              </>
+            ))
+        }
       </tbody>
-    </table>
-  )
+    </table> :
+    <div>Loading...</div>
 }
 
 export default ProteinView;
