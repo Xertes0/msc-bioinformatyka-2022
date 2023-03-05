217ae9820a6f7c664d73c652bbd716d9741ea733
Jonatan Czarniecki <czarniecki.jonatan@gmail.com>
2023-03-05 12:04:13 +0100

    optimize and reformat code, make ORFs selectable

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,42 +1,113 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import SequenceInput from "./SequenceInput";
-import ProteinView from "./ProteinView";
-import {ScrollContainer} from "react-indiana-drag-scroll";
+import SequenceInput from "./components/SequenceInput";
+import AminoAcidView, {
+  OpenReadingFrameViewData,
+} from "./components/AminoAcidView";
+import { ScrollContainer } from "react-indiana-drag-scroll";
+import BioModuleLoad from "@cxx/biolib/bio.mjs";
+import { data } from "./AminoAcidData";
+
+export interface OpenReadingFrameIndicesInfo {
+  nbStart: number;
+  nbEnd: number;
+  aaStart: number;
+  aaEnd: number;
+}
 
 function App() {
-    const [sequence, setSequence] = useState("");
+  const [bioModule, setBioModule] = useState<null | BioModule>(null);
+  const [sequence, setSequence] = useState("");
 
-    function submit(sequence: string) {
-        setSequence(sequence);
-    }
+  useEffect(() => {
+    BioModuleLoad().then((res) => {
+      setBioModule(res);
+    });
+  }, []);
 
-    return (
-        <div className="App">
-            <h1>Input a nucleic acid sequence:</h1>
-            <SequenceInput onSubmit={submit} />
-            <div className="form-description-container">
-                <h2>The input accepts nucleobases mapped as follows:</h2>
-                <ul>
-                    <li>A = Adenine</li>
-                    <li>C = Cytosine</li>
-                    <li>G = Guanine</li>
-                    <li>T = Thymine</li>
-                    <li>U = Uracil</li>
-                </ul>
-            </div>
-            { sequence &&
-                <ScrollContainer
-                  mouseScroll={true}
-                  hideScrollbars={true}
-                  className="protein-view-container">
-                    <ProteinView sequence={sequence} onOpenReadingFrameClick={() => {  }}/>
-                </ScrollContainer>
+  const [orfData, setOrfData] = useState([[], [], []] as [
+    OpenReadingFrameViewData[],
+    OpenReadingFrameViewData[],
+    OpenReadingFrameViewData[]
+  ]);
+
+  const [translatedSequences, setTranslatedSequences] = useState<
+    [string, string, string]
+  >([] as unknown as [string, string, string]);
+
+  useEffect(() => {
+    if (!bioModule) return;
+    let result = bioModule.translate(sequence);
+    setOrfData(
+      result.map((result) => {
+        let indices: OpenReadingFrameViewData[] = [];
+        for (let i = 0; i < result.indices.size(); i++) {
+          const { end, start } = result.indices.get(i);
+          indices[i] = {
+            sequence: Array.from(result.sequence.slice(start, end)).map(
+              (c) => data.find((d) => d.symbol === c)?.abbreviation || "-"
+            ),
+            shift: start,
+          };
+        }
+        return indices;
+      }) as [
+        OpenReadingFrameViewData[],
+        OpenReadingFrameViewData[],
+        OpenReadingFrameViewData[]
+      ]
+    );
+    setTranslatedSequences(
+      (result?.map((v) => v.sequence) as [string, string, string]) || []
+    );
+  }, [sequence, !!bioModule]);
+
+  const [selectedOrf, setSelectedOrf] = useState<[number, number]>();
+
+  function submit(sequence: string) {
+    setSequence(sequence);
+  }
+
+  return (
+    <div className="App">
+      <h1>Input a nucleic acid sequence:</h1>
+      <SequenceInput onSubmit={submit} />
+      <div className="form-description-container">
+        <h2>The input accepts nucleobases mapped as follows:</h2>
+        <ul>
+          <li>A = Adenine</li>
+          <li>C = Cytosine</li>
+          <li>G = Guanine</li>
+          <li>T = Thymine</li>
+          <li>U = Uracil</li>
+        </ul>
+      </div>
+      {sequence && (
+        <ScrollContainer
+          mouseScroll={true}
+          hideScrollbars={true}
+          className="protein-view-container"
+        >
+          <AminoAcidView
+            sequence={Array.from(sequence)}
+            sequences={
+              translatedSequences.map((s) =>
+                Array.from(s).map(
+                  (c) => data.find((d) => d.symbol === c)?.abbreviation || "-"
+                )
+              ) as [string[], string[], string[]]
             }
-
-        </div>
-    )
+            orfData={orfData}
+            onOpenReadingFrameClick={(event) => {
+              setSelectedOrf(event.idx);
+            }}
+            selectedOrf={selectedOrf}
+          />
+        </ScrollContainer>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- web/src/Skeletal.tsx
+++ web/src/Skeletal.tsx
@@ -4,28 +4,36 @@ import { useLocation } from "react-router-dom";
 import BioModuleLoad from "@cxx/biolib/bio.mjs";
 
 export default function Skeletal() {
-    const searchParams = new URLSearchParams(useLocation().search);
-    const formula = searchParams.get("formula");
-    if (!formula) {
-        return <div>Supply `formula` in get parameters.</div>;
-    } else if (formula.length < 1) {
-        return <div>Supplied `formula` is too short.</div>;
-    }
+  const searchParams = new URLSearchParams(useLocation().search);
+  const formula = searchParams.get("formula");
+  if (!formula) {
+    return <div>Supply `formula` in get parameters.</div>;
+  } else if (formula.length < 1) {
+    return <div>Supplied `formula` is too short.</div>;
+  }
 
-    const [formulaSvg, setFormulaSvg] = useState<string | null>(null);
+  const [formulaSvg, setFormulaSvg] = useState<string | null>(null);
 
-    useEffect(() => {
-        BioModuleLoad().then((res) => {
-            setFormulaSvg(res.drawSkeletal(formula));
-        })
-    }, []);
-    return (
-        formulaSvg ?
-            <div className={"Skeletal"}>
-                <h1>Skeletal Formula</h1>
-                <div id="svgDiv" style={{ height: "45vh", maxWidth: "1000vw", margin: "auto", backgroundColor: "white" }}
-                    dangerouslySetInnerHTML={{ __html: formulaSvg }}>
-                </div>
-            </div> : <div>Loading...</div>
-    )
+  useEffect(() => {
+    BioModuleLoad().then((res) => {
+      setFormulaSvg(res.drawSkeletal(formula));
+    });
+  }, []);
+  return formulaSvg ? (
+    <div className={"Skeletal"}>
+      <h1>Skeletal Formula</h1>
+      <div
+        id="svgDiv"
+        style={{
+          height: "45vh",
+          maxWidth: "1000vw",
+          margin: "auto",
+          backgroundColor: "white",
+        }}
+        dangerouslySetInnerHTML={{ __html: formulaSvg }}
+      ></div>
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
 }
